1330f5dff3328d0d30c8458fada5bab685038116
cerega <12345cerega@mail.ru>
2023-12-01 17:19:42 +0300

    fixJs

---
+++ menu/script.js
@@ -0,0 +1,6 @@
+// const btnGame = document.querySelector('.btn-game')
+// let btnOptions = document.querySelector('.btn-options')
+
+// btnGame.addEventListener('click', function () {
+//     window.location.href = "../index.html"
+// })
