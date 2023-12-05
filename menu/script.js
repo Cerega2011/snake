a6380c680d8105939eb4d1d723dd7753b98392dc
cerega <12345cerega@mail.ru>
2023-12-05 16:47:37 +0300

    сделалаи меню и кнопку играть

--- menu/script.js
+++
@@ -1,6 +0,0 @@
-// const btnGame = document.querySelector('.btn-game')
-// let btnOptions = document.querySelector('.btn-options')
-
-// btnGame.addEventListener('click', function () {
-//     window.location.href = "../index.html"
-// })
