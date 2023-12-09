ca2929207561a99daec9c6dcadf5e06290f71348
cerega <12345cerega@mail.ru>
2023-12-09 19:10:01 +0300

    webPack

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+    mode: "production",
+    entry: {
+        menu: "./js/menu.js",
+        game: "./js/game.js",
+    },
+    output: {
+        filename: "[name].bundle.js",
+        path: path.resolve(__dirname, "dist"),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"],
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./index.html",
+            filename: "index.html",
+            chunks: ["menu"],
+        }),
+        new HtmlWebpackPlugin({
+            template: "./game.html",
+            filename: "game.html",
+            chunks: ["game"],
+        }),
+        new CopyWebpackPlugin({
+            patterns: [
+                { from: "css", to: "css" },
+                { from: "images", to: "images" },
+            ],
+        }),
+    ],
+};
